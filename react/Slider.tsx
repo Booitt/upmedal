be665f5a1ec1bed27c8b973bd51f7949e8ec9ad6
unknown <allisson-xd@hotmail.com>
2021-08-17 23:02:00 -0300

    *

--- react/Slider.tsx
+++ react/Slider.tsx
@@ -1,23 +1,56 @@
-import React from "react"
-import Carousel from "@brainhubeu/react-carousel"
-import "./styles.css"
-import img from "../assets/camiseta_gremio.jpg"
-import { useCssHandles } from 'vtex.css-handles'
-
-const CSS_HANDLES = ['slider'] as const
+import React, { useState } from "react";
+import {
+	FormControl,
+	makeStyles,
+	MenuItem,
+	Select,
+} from "@material-ui/core";
 
 interface SliderProps {}
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		color: "white",
+		backgroundColor: "#303030"
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+		color: "white",
+		'select': {
+			backgroundColor: "black"
+		}
+	},
+	background: {
+		backgroundColor: "black"
+	}
+}));
+
 const Slider: StorefrontFunctionComponent<SliderProps> = ({}) => {
-  const handles = useCssHandles(CSS_HANDLES)
+	const classes = useStyles();
+	const [age, setAge] = useState(10);
+	const handleChange = (e: any) => {
+		setAge(e.target.value);
+		console.log(age);
+	};
 	return (
-			<Carousel className={handles.slider} plugins={["arrows"]}>
-				<img src={img} />
-				<img src={img} />
-				<img src={img} />
-			</Carousel>
-	)
-}
+		<>
+			<FormControl variant="filled" className={classes.formControl}>
+				<Select
+					className={classes.root}
+					labelId="demo-simple-select-filled-label"
+					id="demo-simple-select-filled"
+					value={age}
+					onChange={handleChange}
+				>
+					<MenuItem className={classes.root} value={10}>Ten</MenuItem>
+					<MenuItem value={20}>Twenty</MenuItem>
+					<MenuItem value={30}>Thirty</MenuItem>
+				</Select>
+			</FormControl>
+		</>
+	);
+};
 
 // Site editor
 Slider.schema = {
@@ -25,6 +58,6 @@ Slider.schema = {
 	description: "editor.countdown.description",
 	type: "object",
 	properties: {},
-}
+};
 
-export default Slider
+export default Slider;
